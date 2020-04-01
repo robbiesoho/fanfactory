7318692e25bfacb5de65486f2ba83b0395fb5452
RobertP <rob89porto@gmail.com>
2020-04-01 13:03:09 -0400

    Added login function

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,13 @@ Rails.application.routes.draw do
   get  '/help', to: 'static#help'
   get  '/events', to: 'events#index'
   get  '/events/new', to: 'events#new'
+  post '/events/new', to: 'events#create'
   get  '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
  
   resources :events
   resources :admins
   resources :customers
+  resources :users
   
 end
