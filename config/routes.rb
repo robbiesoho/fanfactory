eff5c42b41751942fa6fe118d9511963cba94a67
RobertP <rob89porto@gmail.com>
2020-04-01 16:49:53 -0400

    add user controllerand login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
   get 'users/new'
-  root 'static#static'
+  root 'sessions#new'
   get  '/help', to: 'static#help'
   get  '/events', to: 'events#index'
   get  '/events/new', to: 'events#new'
   post '/events/new', to: 'events#create'
   get  '/signup',  to: 'users#new'
   post '/signup',  to: 'users#create'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
  
   resources :events
   resources :admins
