2c85eb50c459dcb64e39d5ce732bed078cfcec38
RobertP <rob89porto@gmail.com>
2020-04-17 09:40:10 -0400

    fixed bugs

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,5 @@
 class Customer < ApplicationRecord
-  has_many :customerevents, class_name: 'CustomerEvent'
+  has_many :customerevents, class_name: 'CustomerEvent',  dependent: :delete_all
   has_many :events, through: :customerevents
 
 end
