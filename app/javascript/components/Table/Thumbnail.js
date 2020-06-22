62f50f0b0ecb72bd579a8114a30efba8c0489d09
robbiesoho <rob89porto@gmail.com>
2020-06-22 19:56:27 -0400

    trying to mount to heroku

--- app/javascript/components/Table/Thumbnail.js
+++ app/javascript/components/Table/Thumbnail.js
@@ -5,7 +5,6 @@ const Thumbnail = (props) => {
 
   return(
     <div className="container-thumbnail">
-      {/* <iframe className="event-thumbnail" src={`http://localhost:3000/${props.image}`}></iframe> */}
       <img className="event-thumbnail" src={`http://localhost:3000/${props.image}`}></img>
     </div>
   )
