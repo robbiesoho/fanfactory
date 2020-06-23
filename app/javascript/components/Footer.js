28aa0d5b8343899048d204229fd84987af76f8dd
robbiesoho <rob89porto@gmail.com>
2020-06-23 08:29:56 -0400

    deploying to server

--- app/javascript/components/Footer.js
+++ app/javascript/components/Footer.js
@@ -5,8 +5,8 @@ class Footer extends React.Component {
   render () {
     return (
       <React.Fragment>
-      <nav className="navbar fixed-bottom navbar-expand-lg navbar-light bg-dark">
-        <div className="container ">
+      <nav className="footer navbar fixed-bottom navbar-expand-lg navbar-light bg-dark">
+        <div className="container footer-container">
           <li className="navbar-brand footer-text non-hov">RPDEV Media</li>
           <li className="navbar-brand footer-text non-hov">Copyright {String.fromCharCode(169)} 2020 Allrights reserved</li>
           <li className="navbar-brand footer-text footer-link"><a href="http://localhost:3000/login">Inicio de sesión de administrador</a></li>
